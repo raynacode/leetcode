b5c2e072a6ac236540f7ab9ad69698a2529778c3
Ruonan Hao <ruonan.h@gmail.com>
2015-06-22 22:00:09 -0700

    leetcode50

--- 50.cpp
+++ 50.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0)
+        if (n == 0)
             return 1;
         if (x == 0)
             return 0;
-        if(n < 0) {
+        if (n < 0) {
             n = -n;
             x = 1 / x;
         }
