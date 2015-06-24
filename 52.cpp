f08476254c1e1b58b77d7614df532e56d51ece0b
Ruonan Hao <ruonan.h@gmail.com>
2015-06-23 23:10:37 -0700

    leetcode51-52

---
+++ 52.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(vector<string>& v, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < v[i].size(); j++) {
+                if (v[i][j] == 'Q') {
+                    if (j == col || abs(j - col) == (row - i))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    void placeQueen(int& res, vector<string>& v, int k, int& n) {
+        if (k == n) {
+            res++;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (isValid(v, k, i)) {
+                v[k][i] = 'Q';
+                placeQueen(res, v, k + 1, n);
+                v[k][i] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        int res = 0;
+        vector<string> v(n, string(n, '.'));
+        placeQueen(res, v, 0, n);
+        return res;
+    }
+};
