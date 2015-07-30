2331dd75d712b3e89973fe6c2b0ae41e6f0c959a
Ruonan Hao <ruonan.h@gmail.com>
2015-07-29 22:50:45 -0700

    leetcode126-127

--- 126.cpp
+++ 126.cpp
@@ -1,69 +1,68 @@
 class Solution {
 public:
-    vector<string> findStrSet(string s, unordered_set<string>& dict) {
-    	vector<string> result;
-    	for (int i = 0; i < s.size(); i++) {
-    		for (char j = 'a'; j <= 'z'; j++) {
-    			if (j != s[i]) {
-    				string tmp = s;
-    				tmp[i] = j;
-    				if (dict.find(tmp) != dict.end())
-    					result.push_back(tmp);
-    			}
-    		}
-    	}
-    	return result;
+    vector<string> findStrSet(string s, unordered_set<string>& wordDict) {
+        vector<string> strSet;
+        for (int i = 0; i < s.size(); i++) {
+            for (char j = 'a'; j <= 'z'; j++) {
+                if (j != s[i]) {
+                    string tmp = s;
+                    tmp[i] = j;
+                    if (wordDict.find(tmp) != wordDict.end())
+                        strSet.push_back(tmp);
+                }
+            }
+        }
+        return strSet;
     }
     
-    bool findLaddersHelper(unordered_set<string>& level, string& end, unordered_set<string>& dict, unordered_map<string, vector<string> >& nexts) {
-    	if (level.empty())
-    	    return false;
-    	unordered_set<string> next_level;
-    	bool found = false;
-    	for (auto it = level.begin(); it != level.end(); it++)
-    		dict.erase(*it);
-    	for (auto it = level.begin(); it != level.end(); it++) {
-    		vector<string> strSet = findStrSet(*it, dict);
-    		for (int i = 0; i < strSet.size(); i++) {
-    			if (strSet[i] == end) {
-    				found = true;
-    				nexts[*it].push_back(end);
-    			}
-    			else if (!found) {
-    				next_level.insert(strSet[i]);
-    				nexts[*it].push_back(strSet[i]);
-    			}
-    		}
-    	}
-    	return found || findLaddersHelper(next_level, end, dict, nexts);
+    bool findLaddersHelper(unordered_set<string> level, string& end, unordered_set<string>& dict, unordered_map<string, vector<string> >& nexts) {
+        if (level.empty())
+            return false;
+        bool found = false;
+        unordered_set<string> next_level;
+        for (auto it = level.begin(); it != level.end(); it++)
+            dict.erase(*it);
+        for (auto it = level.begin(); it != level.end(); it++) {
+            vector<string> strSet = findStrSet(*it, dict);
+            for (int j = 0; j < strSet.size(); j++) {
+                if (strSet[j] == end) {
+                    found = true;
+                    nexts[*it].push_back(end);
+                }
+                else if (!found) {
+                    next_level.insert(strSet[j]);
+                    nexts[*it].push_back(strSet[j]);
+                }
+            }
+        }
+        return found || findLaddersHelper(next_level, end, dict, nexts);
     }
     
     void findPath(string start, string& end, unordered_map<string, vector<string> >& nexts, vector<string>& path, vector<vector<string> >& res) {
-    	if (start == end) {
-    		res.push_back(path);
-    	}
-    	else {
-    		for (auto it = nexts[start].begin(); it != nexts[start].end(); it++) {
-    			path.push_back(*it);
-    			findPath(*it, end, nexts, path, res);
-    			path.pop_back();
-    		}
-    	}
+        if (start == end) {
+            res.push_back(path);
+            return;
+        }
+        for (auto it = nexts[start].begin(); it != nexts[start].end(); it++) {
+            path.push_back(*it);
+            findPath(*it, end, nexts, path, res);
+            path.pop_back();
+        }
     }
     
     vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
-    	vector<vector<string> > res;
-    	vector<string> path(1, start);
-    	if (start == end) {
-    		res.push_back(path);
-    		return res;
-    	}
-    	dict.insert(end);
-    	unordered_set<string> level;
-    	level.insert(start);
-    	unordered_map<string, vector<string> > nexts;
-    	if (findLaddersHelper(level, end, dict, nexts))
-    		findPath(start, end, nexts, path, res);
-    	return res;
+        vector<vector<string> > res;
+        vector<string> path(1, start);
+        if (start == end) {
+            res.push_back(path);
+            return res;
+        }
+        dict.insert(end);
+        unordered_set<string> level;
+        level.insert(start);
+        unordered_map<string, vector<string> > nexts;
+        if (findLaddersHelper(level, end, dict, nexts))
+            findPath(start, end, nexts, path, res);
+        return res;
     }
 };
--- 127.cpp
+++ 127.cpp
@@ -1,71 +1,59 @@
 class Solution {
 public:
     vector<string> findStrSet(string s, unordered_set<string>& wordDict) {
-    	vector<string> res;
-    	for (int i = 0; i < s.size(); i++) {
-    		for (char j = 'a'; j <= 'z'; j++) {
-    			if (j != s[i]) {
-    				string tmp = s;
-    				tmp[i] = j;
-    				if (wordDict.find(tmp) != wordDict.end())
-    					res.push_back(tmp);
-    			}
-    		}
-    	}
-    	return res;
+        vector<string> strSet;
+        for (int i = 0; i < s.size(); i++) {
+            for (char j = 'a'; j <= 'z'; j++) {
+                if (j != s[i]) {
+                    string tmp = s;
+                    tmp[i] = j;
+                    if (wordDict.find(tmp) != wordDict.end())
+                        strSet.push_back(tmp);
+                }
+            }
+        }
+        return strSet;
     }
-
+    
+    int ladderLengthHelper(vector<pair<string, int> >& start, vector<pair<string, int> >& end, unordered_set<string>& wordDict, unordered_map<string, bool>& sm, unordered_map<string, bool>& em) {
+        if (start.size() > end.size())
+            return ladderLengthHelper(end, start, wordDict, em, sm);
+        if (start.size() == 0)
+            return 0;
+        int size = start.size();
+        for (int i = 0; i < size; i++)
+            wordDict.erase(start[i].first);
+        for (int i = 0; i < size; i++) {
+            pair<string, int> cur = start.front();
+            start.erase(start.begin());
+            vector<string> strSet = findStrSet(cur.first, wordDict);
+            for (int j = 0; j < strSet.size(); j++) {
+                if (em[strSet[j]])
+                    return cur.second + end.back().second;
+                sm[strSet[j]] = true;
+                start.push_back(make_pair(strSet[j], cur.second + 1));
+            }
+        }
+        return ladderLengthHelper(start, end, wordDict, sm, em);
+    }
+    
     int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
-    	if (beginWord == endWord)
-    		return 1;
-    	queue<pair<string, int> > start;
-    	queue<pair<string, int> > end;
-    	start.push(make_pair(beginWord, 1));
-    	end.push(make_pair(endWord, 1));
-    	unordered_map<string, bool> sm;
-    	unordered_map<string, bool> em;
-    	for (auto it = wordDict.begin(); it != wordDict.end(); it++) {
-    		sm[*it] = false;
-    		em[*it] = false;
-    	}
-    	sm[beginWord] = true;
-    	em[endWord] = true;
-    	while (!start.empty() && !end.empty()) {
-    		int slen = start.size();
-    		int elen = end.size();
-    		if (slen <= elen) {
-    			for (int i = 0; i < slen; i++) {
-    				string cur = start.front().first;
-    				int level = start.front().second;
-    				vector<string> strSet = findStrSet(cur, wordDict);
-    				for (int j = 0; j < strSet.size(); j++) {
-    					if (!sm[strSet[j]]) {
-    						sm[strSet[j]] = true;
-    						if (em[strSet[j]])
-    							return level + end.back().second;
-    						start.push(make_pair(strSet[j], level + 1));
-    					}
-    				}
-    				start.pop();
-    			}
-    		}
-    		else {
-    			for (int i = 0; i < elen; i++) {
-    				string cur = end.front().first;
-    				int level = end.front().second;
-    				vector<string> strSet = findStrSet(cur, wordDict);
-    				for (int j = 0; j < strSet.size(); j++) {
-    					if (!em[strSet[j]]) {
-    						em[strSet[j]] = true;
-    						if (sm[strSet[j]])
-    							return level + start.back().second;
-    						end.push(make_pair(strSet[j], level + 1));
-    					}
-    				}
-    				end.pop();
-    			}
-    		}
-    	}
-    	return 0;
+        if (beginWord == endWord)
+            return 1;
+        wordDict.insert(beginWord);
+        wordDict.insert(endWord);
+        unordered_map<string, bool> sm;
+        unordered_map<string, bool> em;
+        for (auto it = wordDict.begin(); it != wordDict.end(); it++) {
+            sm[*it] = false;
+            em[*it] = false;
+        }
+        sm[beginWord] = true;
+        em[endWord] = true;
+        vector<pair<string, int> > start;
+        vector<pair<string, int> > end;
+        start.push_back(make_pair(beginWord, 1));
+        end.push_back(make_pair(endWord, 1));
+        return ladderLengthHelper(start, end, wordDict, sm, em);
     }
 };
